89bffcd20fa8e6ada17716c176ba637bb2237ac5
wukun <charmfocus@gmail.com>
2024-10-31 13:27:48 +0800

    init file

---
+++ src/opts.rs
@@ -0,0 +1,36 @@
+use clap::{arg, Parser};
+use std::path::Path;
+
+#[derive(Debug, Parser)]
+#[command(name = "rcli", version, author)]
+pub struct Opts {
+    #[command(subcommand)]
+    pub cmd: SubCommand,
+}
+
+#[derive(Debug, Parser)]
+pub enum SubCommand {
+    #[command(name = "csv", about = "Show CSV, or convert CSV to JSON")]
+    Csv(CsvOpts),
+}
+
+#[derive(Debug, Parser)]
+pub struct CsvOpts {
+    #[arg(short, long, value_parser = verify_input_file)]
+    pub input: String,
+    #[arg(short, long, default_value = "output.json")]
+    pub output: String,
+    #[arg(short, long, default_value_t = ',')]
+    pub delimiter: char,
+    #[arg(short = 'H', long, default_value_t = true)]
+    pub header: bool,
+}
+
+fn verify_input_file(filename: &str) -> Result<String, String> {
+    if Path::new(filename).exists() {
+        Ok(filename.into())
+    } else {
+        let e = format!("Input file does not exist: {}", filename);
+        Err(e)
+    }
+}
