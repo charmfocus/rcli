92ff42cccc20adaa0d85d0c78bbb5a717240bc97
wukun <charmfocus@gmail.com>
2024-11-01 00:08:44 +0800

    support generic csv to json and yaml output

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,11 @@ use rcli::{process_csv, Opts, SubCommand};
 fn main() -> anyhow::Result<()> {
     let opts = Opts::parse();
     match opts.cmd {
-        SubCommand::Csv(opts) => process_csv(&opts.input, &opts.output)?,
+        SubCommand::Csv(opts) => {
+            let output = opts.output.unwrap_or(format!("output.{}", opts.format));
+
+            process_csv(&opts.input, &output, opts.format)?;
+        }
     }
     Ok(())
 }
--- src/process.rs
+++ src/process.rs
@@ -3,6 +3,9 @@ use std::fs;
 use anyhow::Result;
 use csv::Reader;
 use serde::{Deserialize, Serialize};
+use serde_json::Value;
+
+use crate::opts::OutputFormat;
 
 #[derive(Debug, Deserialize, Serialize)]
 #[serde(rename_all = "PascalCase")]
@@ -16,7 +19,7 @@ struct Player {
     kit: u8,
 }
 
-pub fn process_csv(input: &str, output: &str) -> Result<()> {
+pub fn process_csv(input: &str, output: &str, format: OutputFormat) -> Result<()> {
     let mut reader = Reader::from_path(input)?;
     let mut ret = Vec::with_capacity(128);
     // let records = reader
@@ -24,13 +27,20 @@ pub fn process_csv(input: &str, output: &str) -> Result<()> {
     //     .map(|record| record.unwrap())
     //     .collect::<Vec<Player>>();
 
-    for result in reader.deserialize() {
-        let record: Player = result?;
-        ret.push(record);
+    let headers = reader.headers()?.clone();
+    for result in reader.records() {
+        let record = result?;
+        let json_value = headers.iter().zip(record.iter()).collect::<Value>();
+        ret.push(json_value);
     }
 
-    let json = serde_json::to_string_pretty(&ret)?;
-    fs::write(output, json)?;
+    let content = match format {
+        OutputFormat::Json => serde_json::to_string_pretty(&ret)?,
+        OutputFormat::Yaml => serde_yaml::to_string(&ret)?,
+        OutputFormat::Toml => toml::to_string(&ret)?,
+    };
+
+    fs::write(output, content)?;
     // println!("{:?}", records);
     Ok(())
 }
